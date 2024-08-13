884c5d50ab3ca31470389e40118fbda8968ea736
felipeclarindo <fglpc2@gmail.com>
2024-08-13 10:45:52 -0300

    Começando a estrutura da CLI

---
+++ app/app.py
@@ -0,0 +1,20 @@
+class App:
+    def __init__(self) -> None:
+        pass
+
+    def clear(self):   
+        pass 
+        
+    def menu(self):
+        print("""
+------------------------
+--------- Crud ---------
+------------------------
+[1] Create
+[2] Read
+[3] Update
+[4] Delete
+""")
+
+    def run(self):
+        pass
