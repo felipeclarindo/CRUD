5adb4b6b3c2aba02ee72a6a27cb10667ed983855
felipeclarindo <fglpc2@gmail.com>
2024-08-17 23:38:52 -0300

    Adicionando testes para as validações

--- tests/tests_validations.py
+++ tests/tests_validations.py
@@ -1,15 +1,86 @@
-from ..app.modules.utils import validate_table
+from ..app.modules.validations import (
+    validate_column,
+    validate_table,
+    validate_type,
+    validate_value,
+)
+
+
+def tests_validate_column_passeds():
+    assert validate_column("coluna1") == True
+    assert validate_column("  cioluna2") == True
+    assert validate_column("  columna") == True
+    assert validate_column(" aaa    ") == True
+    assert validate_column("    coluimna") == True
+    assert validate_column("   cikyna ") == True
+
+
+def tests_validate_column_fails():
+    assert validate_column("") == False
+    assert validate_column("") == False
+    assert validate_column("") == False
+    assert validate_column("") == False
+    assert validate_column("") == False
+    assert validate_column("") == False
+    assert validate_column("") == False
+
 
 def tests_validate_table_passeds():
-    assert validate_table("") == True
-    assert validate_table("") == True
-    assert validate_table("") == True
-    assert validate_table("") == True
+    assert validate_table("table") == True
+    assert validate_table("table241") == True
+    assert validate_table("   table") == True
+    assert validate_table("table   ") == True
+    assert validate_table("    table     ") == True
+    assert validate_table("    tab1e241     ") == True
+
 
 def tests_validate_table_fails():
+    assert validate_table("!") == False
     assert validate_table("") == False
-    assert validate_table("") == False
-    assert validate_table("") == False
-    assert validate_table("") == False
-    assert validate_table("") == False
-    
+    assert validate_table("   ") == False
+    assert validate_table("!2141") == False
+    assert validate_table("  %!%@!$@  ") == False
+    assert validate_table("  %!aa$@") == False
+    assert validate_table("  42141$@") == False
+    assert validate_table("    tabl    e     ") == False
+    assert validate_table("    ta  ble   aa    ") == False
+
+
+def tests_validate_type_passeds():
+    assert validate_type("1") == "Int"
+    assert validate_type("2") == "Float"
+    assert validate_type("3") == "String"
+    assert validate_type("4") == "Bool"
+    assert validate_type("5") == "Date"
+
+
+def tests_validate_type_fails():
+    assert validate_type("a") == False
+    assert validate_type(" 8") == False
+    assert validate_type("   46   ") == False
+    assert validate_type("111 ") == False
+    assert validate_type("   !4124@#!") == False
+
+
+def tests_validate_value_passeds():
+    assert validate_value("aaaa") == True
+    assert validate_value("111") == True
+    assert validate_value("a124") == True
+    assert validate_value(" aaaa111") == True
+    assert validate_value("        2222") == True
+    assert validate_value("      111aaa  ") == True
+    assert validate_value("    aaaaaa   ") == True
+
+
+def tests_validate_value_fails():
+    assert validate_value("") == False
+    assert validate_value("!1941") == False
+    assert validate_value("1@#!") == False
+    assert validate_value("12 A") == False
+    assert validate_value("$#@!") == False
+    assert validate_value("!@#!") == False
+    assert validate_value("!@@@@@") == False
+    assert validate_value("%!!%@$!¨&") == False
+    assert validate_value(    "%!!%@$!¨&") == False
+    assert validate_value("%!!%@$!¨&"    ) == False
+    assert validate_value(    "%!!%@$!¨&"       ) == False
